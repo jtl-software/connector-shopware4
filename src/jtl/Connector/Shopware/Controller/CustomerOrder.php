dac8cd070b076dc0959a0c93d050c53435371f89
Daniel Böhmer <daniel.boehmer@jtl-software.com>
2015-08-24 11:40:00 +0200

    removed department support todo notes

--- src/jtl/Connector/Shopware/Controller/CustomerOrder.php
+++ src/jtl/Connector/Shopware/Controller/CustomerOrder.php
@@ -127,8 +127,6 @@ class CustomerOrder extends DataController
                         $order->getShippingAddress()->setStreet($street)
                             ->setExtraAddressLine($extraAddressLine)
                             ->setSalutation(Salutation::toConnector($orderSW['shipping']['salutation']));
-
-                        // @TODO: Firmenzusatz mappen auf department
                     }
 
                     // Adding shipping item
