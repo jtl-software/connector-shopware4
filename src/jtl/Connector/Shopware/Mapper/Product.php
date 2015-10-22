48f6d9637aa1d09477c5f265ba5e67f6a6f33ee4
Daniel Böhmer <daniel.boehmer@jtl-software.com>
2015-10-22 13:54:21 +0200

    Changed product attribute start value from 3 to 4

--- src/jtl/Connector/Shopware/Mapper/Product.php
+++ src/jtl/Connector/Shopware/Mapper/Product.php
@@ -629,7 +629,7 @@ class Product extends DataMapper
             $attributeSW->{$setter}(null);
         }
 
-        $i = 2;
+        $i = 3;
         foreach ($product->getAttributes() as $attribute) {
             if (!$attribute->getIsCustomProperty()) {
                 $i++;
